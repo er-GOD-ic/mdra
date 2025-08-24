6421cdbf7a26cf5769a6c752b59dde3573edb854
er-GOD-ic <kamepower@outlook.jp>
2025-08-24 19:33:39 +0900

    Cmakeを追加

---
+++ build/CMakeFiles/mdra.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mdra.dir/src/mdra.cpp.o"
+  "CMakeFiles/mdra.dir/src/mdra.cpp.o.d"
+  "libmdra.a"
+  "libmdra.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mdra.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
